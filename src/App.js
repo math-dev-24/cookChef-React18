0ebe7dc60c8c50fc7831864b7f9c84d64543712c
Mathieu <mathieu.busse@live.fr>
2023-05-19 17:36:11 +0200

    update title

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import styles from "./App.module.sass";
 import Content from "./pages/homePage/Content";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import AdminPage from "./pages/admin/admin";
 
 
 export default function App() {
     const [page, setPage] = useState('homepage')
+
+    useEffect(()=> {
+        document.timeline = "CookChef"
+    })
 
   return (
       <div className={` bg-slate-100 flex flex-col ${styles.containerApp}`}>
